1e759ab312a07402a27972aa8b3a38f1d487054d
manjunath528 <bmanjunath505@gmail.com>
2023-12-09 20:02:11 +0100

    Hyperparameter Tunning

--- src/utils.py
+++ src/utils.py
@@ -22,13 +22,13 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            '''
+            
             para=param[list(models.keys())[i]]
             
 
@@ -37,7 +37,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
             
 
             model.set_params(**gs.best_params_)
-            '''
+        
             model.fit(X_train,y_train)
 
             #model.fit(X_train, y_train)  # Train model
